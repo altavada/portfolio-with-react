2b723698b545ec5e065363df3ae59d931321d20b
altavada <117209509+altavada@users.noreply.github.com>
2023-04-30 15:13:19 -0400

    Contact section

--- src/components/sections/Contact.js
+++ src/components/sections/Contact.js
@@ -1,13 +1,18 @@
-const styles = {
-    main: {
-      paddingTop: "150px",
-    },
-  };
-  
-  export default function Contact() {
-      return (
-          <div style={styles.main}>
-              Contact
-          </div>
-      )
-  }
+import "../../styles/Body.css";
+
+export default function Contact() {
+  return (
+    <div className="main" id="contacts">
+      <h2 className="bodyheader">Contact Me</h2>
+      <br></br>
+      <img src="/images/grey-phone-icon.jpeg" alt="Phone Icon"></img>
+      <div className="boxes">
+        <a href="mailto:sam.tomaka@gmail.com">Email</a>
+        <a href="https://github.com/altavada">GitHub</a>
+        <a href="https://www.linkedin.com/in/samuel-tomaka-81b08921a/">
+          LinkedIn
+        </a>
+      </div>
+    </div>
+  );
+}
