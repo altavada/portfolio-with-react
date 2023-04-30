3f83bb12df92758a19584c39ad3f204ba0cde0b5
altavada <117209509+altavada@users.noreply.github.com>
2023-04-30 12:54:54 -0400

    Created tab switcher

--- src/components/Section.js
+++ src/components/Section.js
@@ -4,36 +4,23 @@ const styles = {
   },
 };
 
-function Section() {
-  return (
-    <main style={styles.main}>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean id magna
-        id nibh vehicula fringilla eget non felis. Vestibulum condimentum diam
-        sit amet enim vulputate, nec euismod velit tempus. Quisque lacinia mi eu
-        neque dignissim, at placerat nibh maximus. Duis vitae eros ac diam
-        bibendum facilisis. Nullam vel finibus leo. Vestibulum porttitor in nunc
-        in fermentum. Aliquam egestas, enim vel rhoncus aliquet, odio sapien
-        auctor enim, ornare feugiat quam mauris vel metus. Proin lacus ex,
-        semper id tortor nec, laoreet faucibus est. Nulla consequat, arcu id
-        porta vestibulum, erat mauris lobortis lectus, et aliquet eros velit sit
-        amet nibh. Sed ex nibh, tempor eget ullamcorper ullamcorper, dapibus
-        eget enim. Ut id felis libero. Integer viverra risus quis urna
-        hendrerit, non tempor ipsum consequat. Pellentesque vehicula dui ut
-        venenatis commodo. Pellentesque eu rutrum metus, a mattis est. Nulla
-        sodales consequat neque non condimentum. Nullam interdum imperdiet odio,
-        et vehicula mi vestibulum id. Proin id eleifend quam. Maecenas imperdiet
-        id quam eget porttitor. Ut finibus est quis vulputate scelerisque.
-        Vestibulum urna nibh, tristique quis egestas sit amet, ullamcorper eget
-        turpis. Donec commodo ipsum neque, ac feugiat lectus euismod in. Integer
-        convallis augue et erat aliquam luctus. Suspendisse aliquam, quam eget
-        scelerisque posuere, libero enim porttitor massa, et vehicula mi felis
-        eu felis. Proin tempor ante ut purus lobortis aliquet. Vivamus semper
-        mollis lobortis. Nullam eget tortor ut elit ullamcorper aliquet nec
-        vitae tellus. Integer vitae sapien est.
-      </p>
-    </main>
-  );
+function Section({ onTab }) {
+  let whichContent;
+  switch (onTab) {
+    case "portfolio":
+      whichContent = <p>Portfolio</p>;
+      break;
+    case "contact":
+      whichContent = <p>Contact</p>;
+      break;
+    case "resume":
+      whichContent = <p>Resume</p>;
+      break;
+    default:
+      whichContent = <p>About</p>;
+  }
+  console.log(whichContent);
+  return whichContent;
 }
 
 export default Section;
